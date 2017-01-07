494ec6b60cf0b1cc5221cd6110febc5092d967d7
Fabian Michael <hallo@fabianmichael.de>
2017-01-07 14:01:39 +0100

    Add redirect detection.

--- widgets/imagekit/lib/apicrawlerresponse.php
+++ widgets/imagekit/lib/apicrawlerresponse.php
@@ -4,13 +4,43 @@ namespace Kirby\Plugins\ImageKit\Widget;
 
 use Exception;
 use DOMDocument;
+use Kirby;
 use Response;
 use Kirby\Plugins\ImageKit\LazyThumb;
 
 
 class APICrawlerResponse extends \Kirby\Component\Response {
 
+  public function __construct(Kirby $kirby) {
+    parent::__construct($kirby);
+
+    // Register listeners for redirects
+    header_register_callback([$this,'detectRedirectRequest']);
+    register_shutdown_function([$this,'detectRedirectRequest']);
+  }
+
+  public function detectRedirectRequest() {
+    // Redirects should be ignored by the widget, so
+    // overrride a redirect and just return a valid json
+    // response.
+    $redirect = in_array(http_response_code(), [301, 302, 303, 304, 307]);
+    $sent     = headers_sent();
+
+    if($redirect && !$sent) {
+      header_remove('location');
+      echo Response::success(true, [
+        'links'  => [],
+        'status' => LazyThumb::status(),
+      ]);
+      exit;
+    }
+
+  }
+
   public function make($response) {
+    
+    // Try to generate response by calling Kirby’s native
+    // respionse component.
     $html = parent::make($response);
 
     if(!class_exists('\DOMDocument')) {
@@ -40,10 +70,12 @@ class APICrawlerResponse extends \Kirby\Component\Response {
     } catch(Exception $e) {
       return Response::error($e->getMessage(), 500);
     }
-    //return response::error('There was an error parsing Bla', 500);
+
     return Response::success(true, [
       'links'  => array_unique($links),
       'status' => LazyThumb::status(),
     ]);
+
   }
+
 }
